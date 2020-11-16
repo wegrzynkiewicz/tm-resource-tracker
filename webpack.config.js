7bfbb7bb77e866565be1e2b865fcbff69273976d
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-11-16 22:15:17 +0100

    Development

--- webpack.config.js
+++ webpack.config.js
@@ -111,7 +111,7 @@ module.exports = {
                 // }
             ],
             display: 'fullscreen',
-            orientation: 'portrait'
+            orientation: 'any'
         }),
     ],
     watchOptions: {
