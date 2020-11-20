83ebe224751bbbe7d6466d9cfe11a1ea9e0ac771
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-11-20 08:42:11 +0100

    Development

--- docs/main.js
+++ docs/main.js
@@ -7,21 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/styles/_resource.scss":
-/*!***********************************!*\
-  !*** ./src/styles/_resource.scss ***!
-  \***********************************/
-/*! namespace exports */
-/*! exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./src/styles/_resource.scss?");
-
-/***/ }),
 
 /***/ "./src/styles/main.scss":
 /*!******************************!*\
@@ -32,6 +18,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./src/styles/main.scss?");
 
 /***/ }),
@@ -40,12 +27,11 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! namespace exports */
-/*! exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_resource_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/_resource.scss */ \"./src/styles/_resource.scss\");\nlet selectedItem = null;\nlet selectedCounterId = null;\n\n\n\nconsole.log(_styles_resource_scss__WEBPACK_IMPORTED_MODULE_0__.resource);\n\nfunction getCounterValue(name) {\n    const counter = document.getElementById(name);\n    return parseInt(counter.value.toString());\n}\n\nfunction setCounterValue(name, value) {\n    const element = document.getElementById(name);\n    if (element) {\n        element.value = value;\n        localStorage.setItem(name, value);\n    }\n}\n\nfunction addCounterValue(resource, value) {\n    const stateValue = getCounterValue(`state_${resource}`);\n    setCounterValue(`state_${resource}`, stateValue + value);\n}\n\nfunction increaseStateByProduction(resource) {\n    const productionValue = getCounterValue(`production_${resource}`);\n    addCounterValue(resource, productionValue)\n}\n\nfunction restoreState(resource) {\n    setCounterValue(`state_${resource}`, 0);\n    setCounterValue(`production_${resource}`, 0);\n}\n\nconst resources = {\n    points: {\n        restoreState(resource) {\n            setCounterValue(`state_${resource}`, 20);\n        },\n        increaseStateByProduction() {\n        },\n    },\n    gold: {\n        restoreState,\n        increaseStateByProduction(resource) {\n            const productionValue = getCounterValue(`production_${resource}`);\n            const pointsValue = getCounterValue(`state_points`);\n            addCounterValue(resource, productionValue + pointsValue);\n        }\n    },\n    steel: {\n        restoreState,\n        increaseStateByProduction\n    },\n    titan: {\n        restoreState,\n        increaseStateByProduction\n    },\n    plant: {\n        restoreState,\n        increaseStateByProduction\n    },\n    energy: {\n        restoreState,\n        increaseStateByProduction(resource) {\n            const stateValue = getCounterValue(`state_${resource}`);\n            setCounterValue(`state_${resource}`, -stateValue);\n            increaseStateByProduction(resource)\n            addCounterValue('heat', stateValue);\n        }\n    },\n    heat: {\n        restoreState,\n        increaseStateByProduction\n    },\n}\n\nwindow.setSelected = function setSelected(event, type, resource) {\n    if (selectedItem !== null) {\n        selectedItem.classList.remove('resource--item__outline');\n    }\n    selectedItem = this;\n    selectedItem.classList.add('resource--item__outline');\n    selectedCounterId = `${type}_${resource}`;\n}\n\nwindow.setValue = function (event, value) {\n    if (selectedCounterId === null) {\n        return;\n    }\n\n    const stateValue = getCounterValue(selectedCounterId);\n    setCounterValue(selectedCounterId, stateValue + value);\n}\n\nwindow.produce = function (event) {\n    for (const [name, resource] of Object.entries(resources)) {\n        resource.increaseStateByProduction(name);\n    }\n}\n\nwindow.resetStates = function (event) {\n    for (const [name, resource] of Object.entries(resources)) {\n        resource.restoreState(name);\n    }\n}\n\ndocument.addEventListener('DOMContentLoaded', () => {\n    for (const [name] of Object.entries(resources)) {\n        setCounterValue(`state_${name}`, localStorage.getItem(`state_${name}`));\n        setCounterValue(`production_${name}`, localStorage.getItem(`production_${name}`));\n    }\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("let selectedItem = null;\nlet selectedCounterId = null;\n\nfunction getCounterValue(name) {\n    const counter = document.getElementById(name);\n    return parseInt(counter.value.toString());\n}\n\nfunction setCounterValue(name, value) {\n    const element = document.getElementById(name);\n    if (element) {\n        element.value = value;\n        localStorage.setItem(name, value);\n    }\n}\n\nfunction addCounterValue(resource, value) {\n    const stateValue = getCounterValue(`state_${resource}`);\n    setCounterValue(`state_${resource}`, stateValue + value);\n}\n\nfunction increaseStateByProduction(resource) {\n    const productionValue = getCounterValue(`production_${resource}`);\n    addCounterValue(resource, productionValue)\n}\n\nfunction restoreState(resource) {\n    setCounterValue(`state_${resource}`, 0);\n    setCounterValue(`production_${resource}`, 0);\n}\n\nfunction loadCounter(name) {\n    const value = parseInt(localStorage.getItem(name));\n    if (!isNaN(value)) {\n        setCounterValue(name, value);\n    }\n}\n\nconst resources = {\n    points: {\n        restoreState(resource) {\n            setCounterValue(`state_${resource}`, 20);\n        },\n        increaseStateByProduction() {\n        },\n    },\n    gold: {\n        restoreState,\n        increaseStateByProduction(resource) {\n            const productionValue = getCounterValue(`production_${resource}`);\n            const pointsValue = getCounterValue(`state_points`);\n            addCounterValue(resource, productionValue + pointsValue);\n        }\n    },\n    steel: {\n        restoreState,\n        increaseStateByProduction\n    },\n    titan: {\n        restoreState,\n        increaseStateByProduction\n    },\n    plant: {\n        restoreState,\n        increaseStateByProduction\n    },\n    energy: {\n        restoreState,\n        increaseStateByProduction(resource) {\n            const stateValue = getCounterValue(`state_${resource}`);\n            setCounterValue(`state_${resource}`, 0);\n            increaseStateByProduction(resource)\n            addCounterValue('heat', stateValue);\n        }\n    },\n    heat: {\n        restoreState,\n        increaseStateByProduction\n    },\n}\n\nwindow.setSelected = function setSelected(event, type, resource) {\n    if (selectedItem !== null) {\n        selectedItem.classList.remove('resource--item__outline');\n    }\n    selectedItem = this;\n    selectedItem.classList.add('resource--item__outline');\n    selectedCounterId = `${type}_${resource}`;\n}\n\nwindow.setValue = function (event, value) {\n    if (selectedCounterId === null) {\n        return;\n    }\n\n    const stateValue = getCounterValue(selectedCounterId);\n    setCounterValue(selectedCounterId, stateValue + value);\n}\n\nwindow.produce = function (event) {\n    for (const [name, resource] of Object.entries(resources)) {\n        resource.increaseStateByProduction(name);\n    }\n}\n\nwindow.resetStates = function (event) {\n    for (const [name, resource] of Object.entries(resources)) {\n        resource.restoreState(name);\n    }\n}\n\ndocument.addEventListener('DOMContentLoaded', () => {\n    for (const [name] of Object.entries(resources)) {\n        loadCounter(`state_${name}`);\n        loadCounter(`production_${name}`);\n    }\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
@@ -90,7 +76,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 	// startup
 /******/ 	// Load entry module
 /******/ 	__webpack_require__("./src/index.js");
-/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ 	__webpack_require__("./src/styles/main.scss");
+/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
