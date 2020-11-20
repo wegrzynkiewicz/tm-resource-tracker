ca828af613eede59f914e42a5731b3fd47c04d3b
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2020-11-20 10:42:44 +0100

    Development

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let selectedItem = null;
 let selectedCounterId = null;
+let minCounterValue = null;
 
 function getCounterValue(name) {
     const counter = document.getElementById(name);
@@ -15,18 +16,18 @@ function setCounterValue(name, value) {
 }
 
 function addCounterValue(resource, value) {
-    const stateValue = getCounterValue(`state_${resource}`);
-    setCounterValue(`state_${resource}`, stateValue + value);
+    const stateValue = getCounterValue(`state-${resource}`);
+    setCounterValue(`state-${resource}`, stateValue + value);
 }
 
 function increaseStateByProduction(resource) {
-    const productionValue = getCounterValue(`production_${resource}`);
+    const productionValue = getCounterValue(`production-${resource}`);
     addCounterValue(resource, productionValue)
 }
 
 function restoreState(resource) {
-    setCounterValue(`state_${resource}`, 0);
-    setCounterValue(`production_${resource}`, 0);
+    setCounterValue(`state-${resource}`, 0);
+    setCounterValue(`production-${resource}`, 0);
 }
 
 function loadCounter(name) {
@@ -36,44 +37,71 @@ function loadCounter(name) {
     }
 }
 
+function refreshModifier(id, condition) {
+    if (condition) {
+        document.getElementById(id).removeAttribute('disabled');
+    } else {
+        document.getElementById(id).setAttribute('disabled', 'disabled');
+    }
+}
+
+function refreshModifiers() {
+    if (selectedCounterId === null) {
+        return;
+    }
+
+    const stateValue = getCounterValue(selectedCounterId);
+
+    refreshModifier('modifier-minus-ten', stateValue >= 10 + minCounterValue);
+    refreshModifier('modifier-minus-five', stateValue >= 5 + minCounterValue);
+    refreshModifier('modifier-minus-one', stateValue >= 1 + minCounterValue);
+}
+
 const resources = {
     points: {
+        minStateValue: 0,
         restoreState(resource) {
-            setCounterValue(`state_${resource}`, 20);
+            setCounterValue(`state-${resource}`, 20);
         },
         increaseStateByProduction() {
         },
     },
     gold: {
+        minStateValue: -5,
         restoreState,
         increaseStateByProduction(resource) {
-            const productionValue = getCounterValue(`production_${resource}`);
-            const pointsValue = getCounterValue(`state_points`);
+            const productionValue = getCounterValue(`production-${resource}`);
+            const pointsValue = getCounterValue(`state-points`);
             addCounterValue(resource, productionValue + pointsValue);
         }
     },
     steel: {
+        minStateValue: 0,
         restoreState,
         increaseStateByProduction
     },
     titan: {
+        minStateValue: 0,
         restoreState,
         increaseStateByProduction
     },
     plant: {
+        minStateValue: 0,
         restoreState,
         increaseStateByProduction
     },
     energy: {
+        minStateValue: 0,
         restoreState,
         increaseStateByProduction(resource) {
-            const stateValue = getCounterValue(`state_${resource}`);
-            setCounterValue(`state_${resource}`, 0);
+            const stateValue = getCounterValue(`state-${resource}`);
+            setCounterValue(`state-${resource}`, 0);
             increaseStateByProduction(resource)
             addCounterValue('heat', stateValue);
         }
     },
     heat: {
+        minStateValue: 0,
         restoreState,
         increaseStateByProduction
     },
@@ -85,7 +113,18 @@ window.setSelected = function setSelected(event, type, resource) {
     }
     selectedItem = this;
     selectedItem.classList.add('resource--item__outline');
-    selectedCounterId = `${type}_${resource}`;
+    selectedCounterId = `${type}-${resource}`;
+
+    switch (type) {
+        case 'production':
+            minCounterValue = 0;
+            break;
+        case 'state':
+            minCounterValue = resources[resource].minStateValue;
+            break;
+    }
+
+    refreshModifiers();
 }
 
 window.setValue = function (event, value) {
@@ -95,6 +134,8 @@ window.setValue = function (event, value) {
 
     const stateValue = getCounterValue(selectedCounterId);
     setCounterValue(selectedCounterId, stateValue + value);
+
+    refreshModifiers();
 }
 
 window.produce = function (event) {
@@ -111,7 +152,7 @@ window.resetStates = function (event) {
 
 document.addEventListener('DOMContentLoaded', () => {
     for (const [name] of Object.entries(resources)) {
-        loadCounter(`state_${name}`);
-        loadCounter(`production_${name}`);
+        loadCounter(`state-${name}`);
+        loadCounter(`production-${name}`);
     }
 });
